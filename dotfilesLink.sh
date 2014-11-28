4f8b7fa7638c343541899ed3df5978dfdb4703e9
doi toshiya <doi.t.npdp@gmail.com>
2014-11-28 13:35:29 +0900

    add to install the diff-highlight

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ ! -e /usr/local/bin/diff-highlight ]; then
+    wget https://raw.github.com/git/git/master/contrib/diff-highlight/diff-highlight &&\
+    chmod +x diff-highlight &&\
+    sudo mv diff-highlight /usr/local/bin
+fi
+
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-

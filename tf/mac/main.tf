ab91cc720f04af261434b1c6feddea5fe50a632f
Toshiya Doi <doi.t.npdp@gmail.com>
2022-10-01 12:17:52 +0900

    Import install brew packages

--- tf/mac/main.tf
+++ tf/mac/main.tf
@@ -1,5 +1,15 @@
 locals {
-  apps = ["git", "starship"]
+  apps = [
+    "git", "gh", "starship",
+    "go", "pyenv", "tfenv",
+    "bat", "tree",
+
+    # cask
+    "visual-studio-code",
+    # "docker", https://github.com/shihanng/terraform-provider-installer/issues/42
+    "slack", "slack", "zoom",
+    "1password", "todoist", "kindle",
+  ]
 }
 
 resource "installer_brew" "this" {

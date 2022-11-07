995401c8beee343ce5fa404740b1fc8c946d4e5d
Toshiya Doi <doi.t.npdp@gmail.com>
2022-11-07 09:24:37 +0900

    Add a setting for clipboard in nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -108,6 +108,9 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- Clipboard
+vim.opt.clipboard:append { 'unnamedplus' }
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })

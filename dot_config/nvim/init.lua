04349be47d4129be2a5d758eec19be8265e0fd6e
Toshiya Doi <doi.t.npdp@gmail.com>
2022-12-10 17:57:07 +0900

    Tweak vim config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -195,7 +195,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'bash', 'hcl' },
   auto_install = true, -- Auto install parsers, if missing, for the current buffer
   highlight = { enable = true },
   indent = { enable = true },

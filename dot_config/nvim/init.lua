6cf1f86142e8c78f916e70fdc0549874c4d20086
Toshiya Doi <doi.t.npdp@gmail.com>
2023-02-12 12:35:47 +0900

    Customize key maps of find_files and live_grep

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -29,6 +29,18 @@ require('packer').startup(function(use)
 
   -- Fuzzy Finder (files, lsp, etc)
   use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
+  use 'nvim-telescope/telescope-github.nvim' 
+  use {
+    'pwntester/octo.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'kyazdani42/nvim-web-devicons',
+    },
+    config = function ()
+      require"octo".setup()
+    end
+  }
 
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable "make" == 1 }
@@ -169,6 +181,21 @@ require('telescope').setup {
       },
     },
   },
+  pickers = {
+    find_files = {
+      find_command = {
+        "fd",
+        "--type",
+        "f",
+        "--color=never",
+        "--hidden",
+        "--follow",
+        "-E",
+        ".git/*",
+        "--strip-cwd-prefix",
+      },
+    },
+  },
 }
 
 -- Enable telescope fzf native, if installed
@@ -186,10 +213,10 @@ vim.keymap.set('n', '<leader>/', function()
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
 vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = 'Find files via Ctrl-p' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sf', function() require('telescope.builtin').find_files(require('telescope.themes').get_ivy({})) end, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sg', function() require('telescope.builtin').live_grep(require('telescope.themes').get_ivy({})) end, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
 -- [[ Configure Treesitter ]]
